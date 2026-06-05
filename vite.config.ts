af9dc29b6cf52e76dce930ee76c8e5fe2d6af29d
zzhuoz <zhuohongjie@simm.ac.cn>
2026-06-05 06:11:38 +0000

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/TMap/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
